bf147984e5d0490a078cd692f7ac8576f5c22cf8
Burbelag <duduzovmisa@gmail.com>
2021-09-08 21:43:38 +0300

    new bugs every day

--- calculator/Calculator.cs
+++ calculator/Calculator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
 namespace calculator
@@ -14,43 +15,36 @@ namespace calculator
         {
             _numbers = new List<double>();
             _mathChars = new List<char>();
-            string input = InputMethod();
+            string[] input = InputMethod();
             Console.WriteLine(Calculation(input));
         }
 
-        private string InputMethod()
+        private string[] InputMethod()
         {
-            string value= Console.ReadLine();
+            string[] value = Console.ReadLine()?.Split();
             return value;
         }
 
-        private string Calculation(string input)
+        private string Calculation(string[] input)
         {
+            foreach (var str in input)
+            {
+                if (IsNumCheck(str))
+                    _numbers.Add(double.Parse(str));
+
+                if (IsCharCheck(str))
+                    _mathChars.Add(char.Parse(str));
+            }
+
             double answer = Double.NaN;
-
-            string[] numbers = Regex.Split(input, @"\D+"); //как я понимаю, здесь не получается 
-                                                                //вставить дробное число и я не понимаю как сделать нужное регулярное выражение
-                                                                
-            foreach (string value in numbers)
-            {
-                _numbers.Add(int.Parse(value));
-            }
-
-            foreach (var c in input)
-            {
-                if (c.ToString().Contains("+"))
-                    _mathChars.Add(c);
-                else if (c.ToString().Contains("-"))
-                    _mathChars.Add(c);
-                else if (c.ToString().Contains("*"))
-                    _mathChars.Add(c);
-                else if (c.ToString().Contains("/"))
-                    _mathChars.Add(c);
-            }
-
-
             do
             {
+                if (_mathChars.Count + 1 != _numbers.Count)
+                {
+                    Console.WriteLine("Error v uslovii");
+                    break;
+                }
+
                 for (int i = 0; i < _mathChars.Count; i++)
                 {
                     if (_mathChars[i].Equals('/') || _mathChars[i].Equals('*'))
@@ -75,7 +69,7 @@ namespace calculator
                         switch (_mathChars[i])
                         {
                             case '+':
-                                answer = _numbers[i] + _numbers[i+1];
+                                answer = _numbers[i] + _numbers[i + 1];
                                 RemoveInsert(i, answer);
                                 break;
                             case '-':
@@ -91,12 +85,39 @@ namespace calculator
             return answer.ToString();
         }
 
+
         private void RemoveInsert(int i, double answer)
         {
             _numbers.RemoveAt(i);
             _numbers.RemoveAt(i);
             _numbers.Insert(i, answer);
             _mathChars.RemoveAt(i);
+        }
+
+        private bool IsNumCheck(string value)
+        {
+            int intValue;
+            float floatValue;
+            return int.TryParse(value, out intValue) ||
+                   float.TryParse(value, out floatValue);
+        }
+
+        private bool IsCharCheck(string value)
+        {
+            //БАГ ГДЕТА ЗДЕСЬ
+            //РАБОТАЕТ С 10+ ЧИСЛАМИ
+            // НЕ РАБОТАЕТ С 0-9
+            //Я ЕБАЛИ МЕНЯ СОСАЛИ
+            
+            foreach (var charValue in value)
+            {
+                char temp = Convert.ToChar(value);
+                if (char.IsSymbol('-') || char.IsSymbol('+') ||
+                    char.IsSymbol('*') || char.IsSymbol('/'))
+                    return char.TryParse();
+            }
+
+            return false;
         }
     }
 }
